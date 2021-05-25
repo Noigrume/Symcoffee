6d366cbb6a3e772df7e3b87bff49618965f83edf
Lucie <lucie@pop-os.localdomain>
2021-05-25 13:57:09 +0200

    first page

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,18 +1,19 @@
 <?php
 namespace App\Controller;
 
-use http\Env\Request;
-use http\Env\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class TestController{
+    /**
+     * @Route("/", name="index")
+     */
     public function index(){
         dd("youpi");
     }
     /**
-     * Class TestController
-     * @package App\Controller
      * @Route("/test/{age<\d+>?0}", name="test", methods={"GET","POST"}, schemes={"http","https"})
      */
     public function test(Request $request, $age){
