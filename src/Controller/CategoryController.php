95dcffa5ae421f6d8544796e73507c4cf8a72672
Lucie <lucie@pop-os.localdomain>
2021-06-16 15:20:31 +0200

    amélioration de la barre de navigation avec les catégories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -28,7 +28,7 @@ class CategoryController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $category->setSlug(strtolower($slugger->slug($category->getName())));
             $em->persist($category);
             $em->flush();
@@ -53,7 +53,7 @@ class CategoryController extends AbstractController
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
             return $this->redirectToRoute('homepage');
         }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +22,8 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="le nom est obligatoire")
+     * @Assert\Length(min=3, max=255, minMessage="le nom doit contenir au moins 3 caractères")
      */
     private $name;
 
