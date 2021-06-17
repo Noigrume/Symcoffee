d0a56ab8dc97c1f33de7dbdaf3c53116e38823f1
Lucie <lucie@pop-os.localdomain>
2021-06-17 14:39:39 +0200

    Ajout d'éléments de sécurité sur le formulaire de connexion

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\CategoryRepository;
 use Symfony\Component\Form\FormTypeInterface;
 use App\Entity\Category;
 use App\Form\CategoryType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryController extends AbstractController
@@ -49,6 +53,12 @@ class CategoryController extends AbstractController
     public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em)
     {
         $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw new NotFoundHttpException("Catégorie inexistante");
+        }
+
+        //$this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas autorisé à modifier cette catégorie");
 
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,15 +7,19 @@ use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
+    protected $encoder;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
@@ -26,6 +30,27 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        $admin = new User;
+
+        $hash = $this->encoder->encodePassword($admin, "password");
+
+        $admin->setEmail("admin@gmail.com")
+            ->setFullName("admin")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+
+            $hash = $this->encoder->encodePassword($user, "password");
+
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword($hash);
+            $manager->persist($user);
+        }
 
         for ($c = 0; $c < 3; $c++) {
             $category = new Category;
@@ -40,7 +65,7 @@ class AppFixtures extends Fixture
                     ->setSlug(strtolower($this->slugger->slug($product->getName())))
                     ->setCategory($category)
                     ->setShortDescription($faker->paragraph())
-                    ->setMainPicture($faker->imageUrl(400,400, true));
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
                 $manager->persist($product);
             }
 
